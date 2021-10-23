b061431e11ce14ce3b07889062f424f663032070
Stone595 <stone.ruiz2927@stu.mtec.edu>
2021-10-23 15:45:55 -0600

    add base files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+//query selectors
+
+
+//get golf course information from the api   use promise? 
+
+
+//class for the players? 
+  //score will be an array so that you can add up all of the scores
+  
+  //for each player you are wanting to make this class v
+
+class Player{
+  constructor(name, score, handicap){
+    this.name = name; 
+    this.score = score;
+    this.handicap = 0; 
+  }
+}
+//adds all your score to the total
+
+//updates the total 
+
+//checks your score at the end and then alerts the message
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
